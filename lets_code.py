708a32093896ba42429fa31be3edf2ddfb753ae7
DhwaniKariya <67733431+DhwaniKariya@users.noreply.github.com>
2020-07-04 15:08:12 +0530

    lets_code.py

---
+++ lets_code.py
@@ -0,0 +1,17 @@
+list1=["one","two","three","four","five"]
+list2=["C","C++","JAVA","PYTHON"]
+print(list1)
+print(list2)
+tuple1=("a","b","c","d")
+tuple2=("e","f","g","h")
+print(tuple1[1])
+print(tuple2[2])
+dict1={
+    "a" : "apple",
+    "b" : "ball",
+    "c" : "cat",
+    "d" : "dog"
+}
+print(dict1)
+del dict1["b"]
+print(dict1)
